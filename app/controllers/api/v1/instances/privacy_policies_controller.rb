2153b1c03a28bfc9b6def4e0189a3ae867d2d4a6
TykeTC <dev@uestc.social>
2024-10-08 17:20:50 +0200

    [us] expose basic APIs

--- app/controllers/api/v1/instances/privacy_policies_controller.rb
+++ app/controllers/api/v1/instances/privacy_policies_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Api::V1::Instances::PrivacyPoliciesController < Api::V1::Instances::BaseController
+  skip_before_action :require_authenticated_user!
   before_action :set_privacy_policy
 
   def show
